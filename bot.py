346f37f253fdd610c58f8a5600d0bb3a8925978d
AbukaX <abukinvsevolod@gmail.com>
2025-07-23 13:17:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
+import os
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-# Кнопки
+TOKEN = os.getenv("BOT_TOKEN")
+
 keyboard = [["Реферал", "Отмена"]]
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-# Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
 
-# Обработка кнопок
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
@@ -18,17 +18,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif text == "Отмена":
         await update.message.reply_text("Клавиатура скрыта", reply_markup=ReplyKeyboardRemove())
 
-# Основной запуск
 async def main():
-    app = Application.builder().token("ТВОЙ_ТОКЕН_БОТА").build()
+    app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     await app.run_polling()
 
-# Запуск
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
-
